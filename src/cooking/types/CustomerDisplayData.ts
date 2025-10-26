1baae53d7c615724bb8ce9c8af9091f532403b8d
Yiyi Zhang <zhangyiyi0617@gmail.com>
2025-10-25 21:09:46 -0700

    remove position from CustomerDisplayData

--- src/cooking/types/CustomerDisplayData.ts
+++ src/cooking/types/CustomerDisplayData.ts
@@ -2,14 +2,11 @@
  * Data structure for displaying a customer in the view
  */
 export interface CustomerDisplayData {
-    // there are 3 positions in the counter that can be occupied by customers
-    // positions are 0-indexed (0, 1, 2)
-    position: number;
-    
     // A unique identifier assigned to each customer by the cooking game
+    // e.g., if we have in total 3 customers, their IDs might be "customer0", "customer1", "customer2"
     customerId: string;
     
-    // type of the customer
+    // type of the customer, e.g., "mushroom", "rabbit"
     customerType: string;
     
     // patience level percentage (0 - 100)
