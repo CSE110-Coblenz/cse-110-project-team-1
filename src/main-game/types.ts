6828de0010bcc4a0f0af4d50db42b762ec648398
Sid Nair <sidnair@Sids-MacBook-Pro.local>
2025-11-02 20:23:29 -0800

    Addressing CI part 1

--- src/main-game/types.ts
+++ src/main-game/types.ts
@@ -39,3 +39,74 @@ export interface Position {
     x: number;
     y: number;
 }
+
+
+import Konva from 'konva';
+
+export abstract class Controller {
+
+}
+
+export const NPC_WIDTH: number = 32;
+export const NPC_HEIGHT: number = 32;
+
+export const NPC_RED = '#FF0000'; // red box
+export const MAIN_PLAYER_BLUE = '#0000FF'; // red box
+
+export abstract class View {
+    protected layer: Konva.Layer;
+    protected shapes: Konva.Shape[];
+
+    constructor(layer: Konva.Layer) {
+        this.layer = layer;
+        this.shapes = [];
+    }
+
+    protected destroyShapes(): void {
+        for (const shape of this.shapes) {
+            shape.remove();
+            shape.destroy();
+        }
+        this.shapes = [];
+    }
+
+    draw(): void{
+        this.layer.batchDraw();
+    }
+}
+
+export enum Direction {
+    Up = 0,
+    Down = 1,
+    Left = 2,
+    Right = 3,
+}
+
+export enum Species {
+    SPEC1 = "Species 1",
+    SPEC2 = "Species 2",
+    SPEC3 = "Species 3",
+}
+
+export const MaptoNextSpecies = new Map<Species, Species>([
+  [Species.SPEC1, Species.SPEC2],
+  [Species.SPEC2, Species.SPEC3],
+  [Species.SPEC3, Species.SPEC1],
+]);
+
+export const MapToNextDirection = new Map<Direction, Direction>([
+  [Direction.Up, Direction.Left],
+  [Direction.Left, Direction.Down],
+  [Direction.Down, Direction.Right],
+  [Direction.Right, Direction.Up],
+]);
+
+export const ReverseMapToNextDirection = new Map<Direction, Direction>([
+  [Direction.Up, Direction.Right],
+  [Direction.Right, Direction.Down],
+  [Direction.Down, Direction.Left],
+  [Direction.Left, Direction.Up],
+]);
+
+
+export const DEF_PXL_ADV: number = 24;
