6468b2e9d9d7075a9091c59fb5b51170e6b72656
Sid Nair <sidnair@Sids-MacBook-Pro.local>
2025-11-09 14:29:17 -0800

    Player and NPC inherit common Entity

--- src/main-game/NPC/NPCView.ts
+++ src/main-game/NPC/NPCView.ts
@@ -1,6 +1,6 @@
-import { PlayerView } from '../PlayerView';
+import { EntityView } from '../EntityView';
 
-export class NPCView extends PlayerView {
+export class NPCView extends EntityView {
 	constructor(color: string = '#ffcc00') {
 		super();
 		this.color = '#ff0000';
--- src/main-game/PlayerView.ts
+++ src/main-game/PlayerView.ts
@@ -1,31 +1,7 @@
-import Konva from 'konva';
-import { Direction, Position, Viewport } from './types';
+
+import { EntityView } from './EntityView';
 
 // Later we can accept an image and draw that instead.
-export class PlayerView {
-	protected color = '#ffcc00';
+export class PlayerView extends EntityView{
 
-	public draw(
-		target: CanvasRenderingContext2D | Konva.Layer,
-		viewport: Viewport,
-		position: Position,
-		direction: Direction,
-		radius: number,
-	) {
-		// If target looks like a Konva layer, add a simple circle node
-		if (
-			(target as Konva.Layer).getClassName &&
-			(target as Konva.Layer).getClassName() === 'Layer'
-		) {
-			const layer = target as Konva.Layer;
-			const circle = new Konva.Circle({
-				x: position.x - viewport.x,
-				y: position.y - viewport.y,
-				radius,
-				fill: this.color,
-			});
-			layer.add(circle);
-			return;
-		}
-	}
 }
